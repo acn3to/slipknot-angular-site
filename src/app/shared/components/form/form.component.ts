1c920f488c53b0bdd4e07901601ff26fde354a91
acn3to <costanetoow@gmail.com>
2024-08-19 18:04:28 -0300

    feat(component): add form component

---
+++ src/app/shared/components/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent {
+  @Input() title: string = 'Subscribe to our newsletter';
+  @Input() placeholder: string = 'Your email';
+  @Input() buttonLabel: string = 'Subscribe';
+  @Input() buttonType: 'button' | 'submit' | 'reset' = 'submit';
+  @Input() buttonColor: 'primary' | 'secondary' | 'accent' | 'warn' | 'success' = 'primary'; // Removed 'trial'
+  @Input() buttonDisabled: boolean = false;
+  @Input() buttonIcon?: string;
+  @Input() buttonIconPosition: 'left' | 'right' = 'right';
+  @Output() submit = new EventEmitter<string>();
+
+  email: string = '';
+
+  onSubmit() {
+    if (this.email) {
+      this.submit.emit(this.email);
+      this.email = '';
+    }
+  }
+}
