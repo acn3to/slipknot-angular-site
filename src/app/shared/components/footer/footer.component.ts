357ed9c7aeb72226d080f74bcadfa49f3fd6c884
acn3to <costanetoow@gmail.com>
2024-08-19 19:34:21 -0300

    feat(component): implement footer component

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+  socialIcons = [
+    { name: 'facebook', url: 'https://facebook.com', iconClass: 'fab fa-facebook-f' },
+    { name: 'twitter', url: 'https://twitter.com', iconClass: 'fab fa-twitter' },
+    { name: 'instagram', url: 'https://instagram.com', iconClass: 'fab fa-instagram' }
+  ];
 
+  currentYear: number = new Date().getFullYear();
+  isModalVisible: boolean = false;
+
+  openModal() {
+    this.isModalVisible = true;
+  }
+
+  closeModal() {
+    this.isModalVisible = false;
+  }
 }
