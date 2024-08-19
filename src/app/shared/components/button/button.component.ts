bc12f064fb92b70522db2e8205074dbab5de0cc4
acn3to <costanetoow@gmail.com>
2024-08-19 20:47:48 -0300

    fix(style): fix button, footer and header style

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
 export class ButtonComponent {
   @Input() label: string = 'Button';
   @Input() type: 'button' | 'submit' | 'reset' = 'button';
-  @Input() color: 'primary' | 'secondary' | 'accent' | 'warn' | 'success' = 'primary'; // Removed 'trial'
+  @Input() color: 'primary' | 'secondary' | 'accent' | 'warn' | 'success' = 'primary';
   @Input() disabled: boolean = false;
   @Input() icon?: string;
   @Input() iconPosition: 'left' | 'right' = 'right';
