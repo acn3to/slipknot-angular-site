c361db7f9761c92264ddb36592b79f35532e9b6a
acn3to <costanetoow@gmail.com>
2024-08-20 04:12:12 -0300

    refactor(module): move page routing to app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CallbackComponent } from './pages/callback/callback.component';
+import { BandComponent } from './pages/band/band.component';
+import { MusicComponent } from './pages/music/music.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { NewsComponent } from './pages/news/news.component';
+import { EventsComponent } from './pages/events/events.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'band', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: 'music', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: 'photos', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: 'videos', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: 'news', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
-  { path: 'events', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'band', component: BandComponent },
+  { path: 'music', component: MusicComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'events', component: EventsComponent },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { PagesModule } from './pages/pages.module';
-import { RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CallbackComponent } from './pages/callback/callback.component';
+import { BandComponent } from './pages/band/band.component';
+import { MusicComponent } from './pages/music/music.component';
+import { PhotosComponent } from './pages/photos/photos.component';
+import { VideosComponent } from './pages/videos/videos.component';
+import { NewsComponent } from './pages/news/news.component';
+import { EventsComponent } from './pages/events/events.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CallbackComponent,
+    BandComponent,
+    MusicComponent,
+    PhotosComponent,
+    VideosComponent,
+    NewsComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
-    SharedModule,
-    PagesModule,
-    RouterModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
