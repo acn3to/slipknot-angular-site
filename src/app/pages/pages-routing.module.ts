1ea51eb6b056da659db8b27eeec468cf4ce9a3e2
acn3to <costanetoow@gmail.com>
2024-08-19 21:23:59 -0300

    fix(module): fix pages routing module

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,20 +1,42 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AlbumsComponent } from './albums/albums.component';
-import { SongsComponent } from './songs/songs.component';
-import { AboutComponent } from './about/about.component';
+import { RouterModule, Routes, Scroll } from '@angular/router';
+import { BandComponent } from './band/band.component';
+import { MusicComponent } from './music/music.component';
+import { PhotosComponent } from './photos/photos.component';
+import { VideosComponent } from './videos/videos.component';
+import { NewsComponent } from './news/news.component';
+import { EventsComponent } from './events/events.component';
+import { StoreComponent } from './store/store.component';
+import { ViewportScroller } from '@angular/common';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'albums', component: AlbumsComponent },
-  { path: 'songs', component: SongsComponent },
-  { path: 'about', component: AboutComponent },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  { path: 'band', component: BandComponent },
+  { path: 'music', component: MusicComponent },
+  { path: 'photos', component: PhotosComponent },
+  { path: 'videos', component: VideosComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'store', component: StoreComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class PagesRoutingModule { }
+export class PagesRoutingModule {
+  constructor(router: Router, viewportScroller: ViewportScroller) {
+    router.events.pipe(
+      filter((event): event is Scroll => event instanceof Scroll)
+    ).subscribe(event => {
+      if (event.position) {
+        viewportScroller.scrollToPosition(event.position);
+      } else if (event.anchor) {
+        viewportScroller.scrollToAnchor(event.anchor);
+      } else {
+        viewportScroller.scrollToPosition([0, 0]);
+      }
+    });
+  }
+}
